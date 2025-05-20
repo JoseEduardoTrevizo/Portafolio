fbafffd18917d3f686d28802bf21a80c2ffb8f87
EduardoTrevizo <Edwardo_tp@hotmail.com>
2025-05-20 11:43:08 -0600

    moficacion end point portafolio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="Portafolio" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
